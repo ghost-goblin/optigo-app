c766ea779f6e6a017e59ee5341cf83a414940c33
Chloe Urisohn <56723522+ghost-goblin@users.noreply.github.com>
2024-05-04 21:54:37 +0200

    add customer page

--- src/components/CustomerPage.jsx
+++ src/components/CustomerPage.jsx
@@ -13,7 +13,7 @@ const CustomerPage = () => {
   const shopname = useSelector((state) => state.shop.name);
 
   return (
-    <div style={{paddingTop:'2rem'}}>
+    <div style={{paddingTop:'2rem', textAlign:'left'}}>
     <Container>
       <h1>Why people are raving about {shopname}</h1>
       <Row xs={1} md={3} className="g-4" style={{paddingTop:'2rem'}}>
@@ -21,12 +21,11 @@ const CustomerPage = () => {
           <Card>
             <Card.Img variant="top" src={photo1} />
             <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+              <Card.Text  className="blockquote">
+               {shopname} instantly replaced my old glasses.
+               Hard to believe something so easy and so fast can add so much style but it's a real game changer.
               </Card.Text>
+              <Card.Title className="blockquote-footer mb-0 font-italic">Jessica</Card.Title>
             </Card.Body>
           </Card>
         </Col>
@@ -34,12 +33,12 @@ const CustomerPage = () => {
           <Card>
             <Card.Img variant="top" src={photo2} />
             <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+              <Card.Text className="blockquote">
+              Every time I use {shopname} I smile.
+              It has made getting new glasses so much fun.
+              As someone who constantly changes frames, {shopname} is a must-have!
               </Card.Text>
+              <Card.Title className="blockquote-footer mb-0 font-italic">Adrian</Card.Title>
             </Card.Body>
           </Card>
         </Col>
@@ -47,12 +46,12 @@ const CustomerPage = () => {
           <Card>
             <Card.Img variant="top" src={photo3} />
             <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+              <Card.Text className="blockquote">
+               {shopname} is an amazing and friendly opticians.
+               They offer stylish and customisable frames with any prescription that you require.
+               And they deliver!
               </Card.Text>
+              <Card.Title className="blockquote-footer mb-0 font-italic">Elliot</Card.Title>
             </Card.Body>
           </Card>
         </Col>
